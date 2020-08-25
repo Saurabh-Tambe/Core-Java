00fcd9e5d4b535be060b372c108aa32105e4cca5
Saurabh Tambe <43045107+Saurabh-Tambe@users.noreply.github.com>
2020-08-25 18:07:01 +0530

    Create Shape.java

---
+++ Interface-Assignment/Shape.java
@@ -0,0 +1,20 @@
+
+public class Shape {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public Shape(String name) {
+	super();
+	this.name = name;
+}
+
+public Shape()
+{}
+}
