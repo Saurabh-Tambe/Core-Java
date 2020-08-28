d69658f66c605d89f5118696f321321140320d3c
Saurabh Tambe <43045107+Saurabh-Tambe@users.noreply.github.com>
2020-08-28 09:27:35 +0530

    Create Address.java

---
+++ File-IO-List-Assignment/Address.java
@@ -0,0 +1,39 @@
+package com.pojo;
+import java.io.Serializable;
+
+
+public class Address implements Serializable {
+private String city;
+private String state;
+private int pincode;
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+public int getPincode() {
+	return pincode;
+}
+public void setPincode(int pincode) {
+	this.pincode = pincode;
+}
+public Address()
+{}
+public Address(String city, String state, int pincode) {
+	super();
+	this.city = city;
+	this.state = state;
+	this.pincode = pincode;
+}
+@Override
+public String toString() {
+	return "Address [city_name=" + city + ", pincode=" + pincode + ", state=" + state + "]";
+}
+}
